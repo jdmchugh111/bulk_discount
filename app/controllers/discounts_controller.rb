5d6c45e9b550bad7759d4d512288a551cb8617c5
James McHugh <jdmchugh111@gmail.com>
2024-06-09 01:18:18 -0400

    Wrote and Passed Tests for User Story 2

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,9 +1,29 @@
 class DiscountsController < ApplicationController
+  # before_action :find_merchant, only: [:new, :create, :index]
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show
     @discount = Discount.find(params[:id])
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    discount = Discount.new(percent_discount: (params[:percent_discount].to_f / 100),
+                    threshold: params[:threshold],
+                    merchant_id: @merchant.id)
+    if discount.save
+      flash.notice = "Succesfully Created Discount"
+      redirect_to merchant_discounts_path(@merchant)
+    else
+      flash.notice = "All fields must be completed"
+      redirect_to new_merchant_discount_path(@merchant)
+    end
+    
   end
 end
