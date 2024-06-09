326535b72cfc6a82ed056d354d03475d7ff34e4c
James McHugh <jdmchugh111@gmail.com>
2024-06-09 00:05:33 -0400

    Wrote and passed tests for User Story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,4 @@ Rake::Task["csv_load:all"].invoke
 @discount1 = Discount.create(percent_discount: 0.10, threshold: 4, merchant_id: 1)
 @discount2 = Discount.create(percent_discount: 0.05, threshold: 6, merchant_id: 1)
 @discount3 = Discount.create(percent_discount: 0.25, threshold: 9, merchant_id: 1)
+@discount4 = Discount.create(percent_discount: 0.30, threshold: 10, merchant_id: 2)
