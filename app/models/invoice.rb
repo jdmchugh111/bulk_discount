69c2e7d6e1f30903b12ce65fd88d15c913547b86
James McHugh <jdmchugh111@gmail.com>
2024-06-09 09:06:28 -0400

    Tried some stuff - not positive on US6 query. Still need to write more tests

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -13,4 +13,29 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  # def total_discount
+  #   invoice_items
+  #     .joins(item: {merchant: :discounts})
+  #     .where("invoice_items.quantity >= discounts.threshold")
+  #     .select("invoice_items.*, max(discounts.percent_discount) as best_discount")
+  #     .group("invoice_items.id")
+  #     .sum("best_discount / 100 * invoice_items.unit_price * quantity")
+  # end
+
+  def total_discount
+    invoice_items
+      .joins(item: { merchant: :discounts })
+      .where("invoice_items.quantity >= discounts.threshold")
+      .group("invoice_items.id")
+      .pluck("invoice_items.id, max(discounts.percent_discount) as best_discount, 
+              invoice_items.unit_price, invoice_items.quantity")
+      .sum do |item_id, best_discount, unit_price, quantity|
+        (best_discount / 100.0) * unit_price * quantity
+      end
+  end
+  
+  def discounted_revenue
+    total_revenue - total_discount
+  end
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -14,4 +14,12 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def discount_applied(quantity)
+    discount = item.merchant.discounts
+              .where("threshold <= ?", quantity)
+              .order(percent_discount: :desc)
+              .first
+    discount.id if discount
+  end
 end
